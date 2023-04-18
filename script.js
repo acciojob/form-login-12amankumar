8a76aaa5572dc3ad2f6a8bf08aed74718712bc9d
12amankumar <117712947+12amankumar@users.noreply.github.com>
2023-04-18 18:04:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getFormvalue() {
     //Write your code here
-	let Name_value=document.querySelector("input")[0].value;
-	let Name_value1=document.querySelector("input")[1].value;
-     let Name_value2=document.querySelector("input")[2].value;
-	alert(Name_value + Name_value1);
+	let Name_value=document.querySelectorAll("input")[0].value;
+	let Name_value1=document.querySelectorAll("input")[1].value;
+     // let Name_value2=document.querySelector("input")[2].value;
+	alert(Name_value +" "+ Name_value1);
 }
